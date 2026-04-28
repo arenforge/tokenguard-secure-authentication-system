d2d94a948d469312c3d017d56f7f985bba7f4cc0
Arhan-Git07 <arhankhancse@gmail.com>
2026-04-28 15:37:44 +0530

    Updated API URL

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 // API Configuration - Change this when deploying!
 var API_URL = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
     ? "http://localhost:8000"
-    : "https://user-auth-final.onrender.com";
+    : "https://tokenguard-secure-authentication-system-production.up.railway.app";
